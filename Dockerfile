3f8a89f12fc4ee685aaa2935096016ff83c54876
Sanket Modi <sankemod@in.ibm.com>
2020-12-21 17:32:11 +0530

    Latest code

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM docker.io/node:10
 
 WORKDIR /usr/src/app
 
+RUN chgrp -R 0 /usr/src & chmod -R u=g /usr/src
+
 EXPOSE 3000
 
 COPY . .
--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const fs = require("fs")
 const app = express()
 const port = 3000
 
+var res1 = ""
+
+fs.readFile('/opt/app-root/secure1/mysecret.sec', 'utf8', function (secerr,secdata) {
+  res1 = res1 + " " + secdata;
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  var response = process.env.APP_MSG + res1;
+
+  // fs.readFile('/opt/app-root/secure1/mysecret.sec', 'utf8', function (secerr,secdata) {
+  //  response = response + " " + secdata;
+  // })
+
+  res.send(response)
 })
 
 app.listen(port, () => {
