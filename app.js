26ccf0acf9ce76ae958715321adf6f976aa24055
Sanket Modi <sankemod@in.ibm.com>
2020-12-22 14:39:19 +0530

    added health and live

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ const fs = require("fs")
 const app = express()
 const port = 3000
 
-var res1 = ""
+//var res1 = ""
 
-fs.readFile('/opt/app-root/secure1/mysecret.sec', 'utf8', function (secerr,secdata) {
-  res1 = res1 + " " + secdata;
-})
+//fs.readFile('/opt/app-root/secure1/mysecret.sec', 'utf8', function (secerr,secdata) {
+//  res1 = res1 + " " + secdata;
+//})
 
 app.get('/', (req, res) => {
-  var response = process.env.APP_MSG + res1;
+  //var response = process.env.APP_MSG + res1;
 
   // fs.readFile('/opt/app-root/secure1/mysecret.sec', 'utf8', function (secerr,secdata) {
   //  response = response + " " + secdata;
   // })
 
-  res.send(response)
+  res.send("Hello Sanket !!")
 })
+
+app.get('/ready', (req, res) => {
+  res.send("ok");
+})
+
+app.get('/live', (req, res) => {
+  res.send("ok");
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
